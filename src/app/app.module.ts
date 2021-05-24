890340c184ae088c85a28a0151c92d2201ac0e86
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-05-24 11:03:46 +0300

    Adding the auto generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
